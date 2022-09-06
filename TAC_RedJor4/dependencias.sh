1451789a0751682ef1195b37ec7b9d7ad9c48f49
Guilherme B <38567020+gui1080@users.noreply.github.com>
2022-09-06 17:45:47 -0300

    Update dependencias.sh

--- TAC_RedJor4/dependencias.sh
+++ TAC_RedJor4/dependencias.sh
@@ -1,14 +1,9 @@
 # Script shell para instalação de dependências
 
 echo "Hello $USER"
-echo "Se certifique que seu Python3 está em dia (Python 3.9.7)"
+echo "Se certifique que seu Python3 está em dia (pelo menos Python 3.9.7)"
 python3 --version
 pip3 install --upgrade pip3
-echo "Instalando ambiente virtual"
-pip3 install virtualenv
-python3 -m venv venv
-echo "Para rodar se deve rodar -> 'source venv/bin/activate'"
-source venv/bin/activate
 python3 manage.py makemigrations
 python3 manage.py migrate
 pip3 install djangorestframework
