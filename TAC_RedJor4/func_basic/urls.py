e5c62c75bd4ce562f1cf4afdca34a3aec23110fd
Guilherme Braga <guib545@aluno.unb.br>
2022-09-16 10:17:40 -0300

    site na branch gh-pages

--- TAC_RedJor4/func_basic/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('cadastro', CadastroView.as_view()), 
-    path('text_news', TextView.as_view()), 
-    path('photo_news', PhotoView.as_view()), 
-    path('graphic_news', GraphicsView.as_view()), 
-]
